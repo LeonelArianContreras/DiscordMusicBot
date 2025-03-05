7754939aabb42c0ef1ba2a1f47a7aec9a7706a54
Leonel Contreras <lecontreras@frba.utn.edu.ar>
2025-03-05 02:15:38 -0300

    Upgrade: Encripted token with .env && Successfull connection

--- src/main/java/dev/discordMusicBot/bot/MusicBot.java
+++ src/main/java/dev/discordMusicBot/bot/MusicBot.java
@@ -5,12 +5,23 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import java.util.EnumSet;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import javax.security.auth.login.LoginException;
 
 public class MusicBot {
+
+    private String token;
+
+    public MusicBot() {
+        Dotenv dotenv = Dotenv.load();  // Load .env
+        this.token = dotenv.get("TOKEN");  // Search "TOKEN" in the file and save the encripted value into token
+    }
+
     public static void main(String[] args) throws LoginException {
-        JDABuilder jda = JDABuilder.createDefault(ERROR,
+        MusicBot bot = new MusicBot();
+
+        JDABuilder jda = JDABuilder.createDefault(bot.token,
                 EnumSet.of(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT));
         jda.setActivity(Activity.listening("<< Musica >>"));
         jda.setStatus(OnlineStatus.ONLINE);
