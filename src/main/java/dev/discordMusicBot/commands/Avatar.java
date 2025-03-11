afb786f04840f598575e2d78f3ecd1179a44c0ff
Leonel Contreras <lecontreras@frba.utn.edu.ar>
2025-03-10 23:53:13 -0300

    Upgrade: commands tagging members

--- src/main/java/dev/discordMusicBot/commands/Avatar.java
+++ src/main/java/dev/discordMusicBot/commands/Avatar.java
@@ -4,15 +4,29 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Avatar implements Command {
 
     public void execute(MessageReceivedEvent event, String[] args) {
-        String avatarUrl = event.getAuthor().getAvatarUrl() + "?size=1024";
+        if(args.length < 2) {
+            event.getChannel().sendMessageEmbeds(this.getEmbedAvatar(event.getMember()).build()).queue();
+            return;
+        }
+        List<Member> taggedMembers = event.getMessage().getMentions().getMembers();
+        List<EmbedBuilder> avatars = taggedMembers.stream().map(Avatar::getEmbedAvatar).collect(Collectors.toList());
+        avatars.forEach(avatar -> event.getChannel().sendMessageEmbeds(avatar.build()).queue());
+    }
+
+    public static EmbedBuilder getEmbedAvatar(Member member) {
+        String avatarUrl = member.getUser().getAvatarUrl() + "?size=1024";
+
         EmbedBuilder embed = new EmbedBuilder(); // Embed lets us beautify the bot interactions
-        embed.setTitle("Avatar de: " + event.getAuthor().getEffectiveName());
+        embed.setTitle("Avatar de: " + member.getEffectiveName());
         embed.setImage(avatarUrl);
         embed.setColor(0x1DA1F2);
 
-        event.getChannel().sendMessageEmbeds(embed.build()).queue();
+        return embed;
     }
 }
--- src/main/java/dev/discordMusicBot/commands/UserInfo.java
+++ src/main/java/dev/discordMusicBot/commands/UserInfo.java
@@ -1,36 +1,55 @@
 package dev.discordMusicBot.commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import net.dv8tion.jda.api.entities.Role;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserInfo implements Command {
 
     public void execute(MessageReceivedEvent event, String[] args) {
-        Member user = event.getMember();
-        EmbedBuilder eb = new EmbedBuilder();
 
-        String username = event.getAuthor().getName();
-        String nickname = event.getAuthor().getEffectiveName();
-        String avatar = event.getAuthor().getAvatarUrl() + "?size=1024";
-        String userid = event.getAuthor().getId();
-        OffsetDateTime joinDate = user.getTimeJoined();
+        if(args.length < 2) {
+            event.getChannel().sendMessageEmbeds(userData(event.getMember()).build()).queue();
+            return;
+        }
+
+        List<Member> taggedUsers = event.getMessage().getMentions().getMembers();
+        List<EmbedBuilder> embedsUser = taggedUsers.stream().map(UserInfo :: userData).collect(Collectors.toList());
+        embedsUser.forEach(embed -> event.getChannel().sendMessageEmbeds(embed.build()).queue());
+    }
+
+    public static EmbedBuilder userData(Member member) {
+        String username = member.getUser().getName();
+        String nickname = member.getEffectiveName();
+        String avatar = member.getUser().getAvatarUrl() + "?size=1024";
+        String userId = member.getId();
+        OffsetDateTime joinDate = member.getTimeJoined();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-        Guild guild = event.getGuild();
-        
+        String roles;
+
+        if(member.getRoles().isEmpty()) {
+            roles = "None";
+
+        } roles = member.getRoles().stream().map(Role :: getName).collect(Collectors.joining(", "));
+
+
+        EmbedBuilder eb = new EmbedBuilder();
         eb.setTitle("User Info");
         eb.setThumbnail(avatar);
         eb.addField("Nickname", nickname, false);
         eb.addField("Username", username, false);
-        eb.addField("UserID", userid, false);
+        eb.addField("UserID", userId, false);
         eb.addField("Date Joined", joinDate.format(formatter), false);
-        eb.addField("Roles", user.getRoles().toString(), false);
+        eb.addField("Roles", roles, false);
         eb.setColor(0x1DA1F2);
 
-        event.getChannel().sendMessageEmbeds(eb.build()).queue();
+        return eb;
     }
 }
