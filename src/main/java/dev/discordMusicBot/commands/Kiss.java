6135558d510abf86be67e58da4f659eae899e490
Leonel Contreras <lecontreras@frba.utn.edu.ar>
2025-03-11 00:47:59 -0300

    Upgrade: gifs resources so as to implement new commands

---
+++ src/main/java/dev/discordMusicBot/commands/Kiss.java
@@ -0,0 +1,10 @@
+package dev.discordMusicBot.commands;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class Kiss implements Command {
+
+    public void execute(MessageReceivedEvent event, String[] args) {
+
+    }
+}
